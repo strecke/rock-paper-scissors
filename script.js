5feca863cb3e4581f0a10de24e462dba7c9c1128
strecke <35091155+strecke@users.noreply.github.com>
2026-03-22 04:46:53 +0100

    Add getHumanChoice function

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
 function getComputerChoice() {
     return ['r', 'p', 's'][Math.floor(Math.random() * 3)];
 }
+
+function getHumanChoice(){
+    const choice = prompt('Choose rock: r, paper: p, scissors: s').toLowerCase();
+    if (choice.length !== 1 || !/^[rps]$/.test(choice)) return;
+    return choice;
+}
