d05c9b95dde5793bfc8a9359c528f99a992191b2
strecke <35091155+strecke@users.noreply.github.com>
2026-03-22 04:47:42 +0100

    Add playRound function

--- script.js
+++ script.js
@@ -7,3 +7,29 @@ function getHumanChoice(){
     if (choice.length !== 1 || !/^[rps]$/.test(choice)) return;
     return choice;
 }
+
+let humanScore = 0;
+let computerScore = 0;
+
+const BEATS = { r: 's', p: 'r', s: 'p', };
+const NAMES = { r: 'Rock', p: 'Paper', s: 'Scissors', };
+
+function playRound(humanChoice, computerChoice) {
+    if (humanChoice === undefined) return;
+    let message;
+    if (humanChoice === computerChoice) {
+        message = `Tie! ${NAMES[humanChoice]} equals ${NAMES[computerChoice]}`
+    } else {
+        const isHumanWinner = BEATS[humanChoice] === computerChoice;
+        message = isHumanWinner
+        ? `You Win! ${NAMES[humanChoice]} beats ${NAMES[computerChoice]}`
+        : `You Lose! ${NAMES[computerChoice]} beats ${NAMES[humanChoice]}`;
+    }
+    
+    console.log(message);
+}
+
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+playRound(humanSelection, computerSelection);
